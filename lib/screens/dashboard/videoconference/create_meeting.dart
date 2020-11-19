5c6be4e4c704d8aeadd8be4b570c4b6d81a3a4cc
Gunjan Shakya <gunjanshakya@pop-os.localdomain>
2020-11-19 15:56:25 +0545

    Create meeting & join meeting

--- lib/screens/dashboard/videoconference/create_meeting.dart
+++ lib/screens/dashboard/videoconference/create_meeting.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:i_moon/constants.dart';
+import 'package:jitsi_meet/feature_flag/feature_flag_enum.dart';
+import 'package:jitsi_meet/jitsi_meet.dart';
+import 'package:jitsi_meet/jitsi_meeting_listener.dart';
+import 'package:jitsi_meet/room_name_constraint.dart';
+import 'package:jitsi_meet/room_name_constraint_type.dart';
 
 class CreateMeeting extends StatefulWidget {
   @override
@@ -7,67 +14,186 @@ class CreateMeeting extends StatefulWidget {
 }
 
 class _CreateMeetingState extends State<CreateMeeting> {
-  String code = "8sk022";
+  String companyName = "iMoon";
+  String roomCode = "8sk022";
+  final nameText = TextEditingController();
+  final subjectText = TextEditingController();
+  var isAudioMuted = false;
+  var isVideoMuted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    JitsiMeet.addListener(JitsiMeetingListener(
+        onConferenceWillJoin: _onConferenceWillJoin,
+        onConferenceJoined: _onConferenceJoined,
+        onConferenceTerminated: _onConferenceTerminated,
+        onError: _onError));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    JitsiMeet.removeAllListeners();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            child: Text(
-              "Create a meeting (Auto-generated code)",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: kPrimaryColor,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                "Code: ",
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Text(
+                "Create a meeting (Auto-generated code)",
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: kPrimaryColor,
                 ),
+                textAlign: TextAlign.center,
               ),
-              Text(
-                "$code",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              )
-            ],
-          ),
-          SizedBox(height: 25),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            width: size.width * 0.8,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(29),
-              child: FlatButton(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                color: kPrimaryColor,
-                onPressed: () {},
-                child: Text(
-                  "Create meeting code",
-                  style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                controller: nameText,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Full name",
+                  labelStyle: TextStyle(
+                      fontSize: 20,
+                      color: kPrimaryColor,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 40),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                controller: subjectText,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Subject name",
+                  labelStyle: TextStyle(
+                      fontSize: 20,
+                      color: kPrimaryColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Code: ",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: kPrimaryColor,
+                  ),
+                ),
+                Text(
+                  "$roomCode",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 25),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              width: size.width * 0.8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(29),
+                child: FlatButton(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  color: kPrimaryColor,
+                  onPressed: () {
+                    _createMeeting();
+                  },
+                  child: Text(
+                    "Create meeting code",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  _createMeeting() async {
+    try {
+      Map<FeatureFlagEnum, bool> featureFlags = {
+        FeatureFlagEnum.WELCOME_PAGE_ENABLED: false,
+      };
+
+      if (Platform.isAndroid) {
+        featureFlags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
+      } else if (Platform.isIOS) {
+        featureFlags[FeatureFlagEnum.PIP_ENABLED] = false;
+      }
+
+      var options = JitsiMeetingOptions()
+        ..room = companyName + roomCode
+        ..subject = subjectText.text
+        ..audioMuted = isAudioMuted
+        ..videoMuted = isVideoMuted
+        ..featureFlags.addAll(featureFlags);
+
+      debugPrint("JitsiMeetingOptions: $options");
+      await JitsiMeet.joinMeeting(
+        options,
+        listener: JitsiMeetingListener(onConferenceWillJoin: ({message}) {
+          debugPrint("${options.room} will join with message: $message");
+        }, onConferenceJoined: ({message}) {
+          debugPrint("${options.room} joined with message: $message");
+        }, onConferenceTerminated: ({message}) {
+          debugPrint("${options.room} terminated with message: $message");
+        }),
+      );
+    } catch (error) {
+      debugPrint("error: $error");
+    }
+  }
+
+  static final Map<RoomNameConstraintType, RoomNameConstraint>
+      customContraints = {
+    RoomNameConstraintType.MAX_LENGTH: new RoomNameConstraint((value) {
+      return value.trim().length <= 50;
+    }, "Maximum room name length should be 30."),
+    RoomNameConstraintType.FORBIDDEN_CHARS: new RoomNameConstraint((value) {
+      return RegExp(r"[$€£]+", caseSensitive: false, multiLine: false)
+              .hasMatch(value) ==
+          false;
+    }, "Currencies characters aren't allowed in room names."),
+  };
+
+  void _onConferenceWillJoin({message}) {
+    debugPrint("_onConferenceWillJoin broadcasted with message: $message");
+  }
+
+  void _onConferenceJoined({message}) {
+    debugPrint("_onConferenceJoined broadcasted with message: $message");
+  }
+
+  void _onConferenceTerminated({message}) {
+    debugPrint("_onConferenceTerminated broadcasted with message: $message");
+  }
+
+  _onError(error) {
+    debugPrint("_onError broadcasted: $error");
   }
 }
